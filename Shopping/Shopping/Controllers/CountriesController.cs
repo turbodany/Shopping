f3bc3235e8ee650e5d350e123ff26ac2a4885ba9
turbodany <USUARIO@DESKTOP-Q33SBE1>
2022-11-25 18:29:28 -0500

    buttonscolor

---
+++ Shopping/Shopping/Controllers/CountriesController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Data;
+using Shopping.Data.Entities;
+
+namespace Shopping.Controllers
+{
+    public class CountriesController : Controller //una clase llamada countriescontroler que hereda de una clase Controller
+    {
+        private readonly DataContext _context;  // un atributo privado _context que representa todo el contexto de datos,
+
+        public CountriesController(DataContext context) //por el constructor del countriescontroller le estamos mandando el contexto de datos, osea el acceso a la db
+        {
+            _context = context; // esto se conoce como una inyeccion de dependencias, luego ese parametro se lo asignamos a un atributo para poderlo utilizar en todo el controlador
+        }
+
+        // GET: Countries
+        public async Task<IActionResult> Index() // las acciones son metodos que devuelven un IActionResult que es una interfaz=se deja pintar en pantalla  
+        {
+            return View(await _context.Countries.ToListAsync()); // devuelve un metodo asincrono(devuelve una promesa que para que sea bien ejecutado se necesita utilizar el await  
+        }
+
+        // GET: Countries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // GET: Countries/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Countries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(country);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return View(country);
+        }
+
+        // POST: Countries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Country country)
+        {
+            if (id != country.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(country);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CountryExists(country.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Countries == null)
+            {
+                return Problem("Entity set 'DataContext.Countries'  is null.");
+            }
+            var country = await _context.Countries.FindAsync(id);
+            if (country != null)
+            {
+                _context.Countries.Remove(country);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id  { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caracterés.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } 
+
+    }
+}
